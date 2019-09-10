40c48df4998d6f479a9884a6ed17af1f28133f4d
nabeel huter <nabeelhuter@Nabeels-MBP.lan1>
2019-09-10 15:34:40 -0400

    first commit

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int [] arrayOfNumbers = {22,1,33,42,4,7,2,9};
+
+        for (int i=1;i<arrayOfNumbers.length;i++){
+
+            int j = i-1;
+                    int point = arrayOfNumbers[i];
+
+            while (j >= 0 && arrayOfNumbers[j] > point){
+                arrayOfNumbers [j+1] = arrayOfNumbers [j];
+                j = j - 1;
+            }
+
+            arrayOfNumbers[j+1] = point;
+        }
+
+        for (int x = 0; x < arrayOfNumbers.length; x++){
+            System.out.println(arrayOfNumbers[x]);
+        }
+    }
+
+}
